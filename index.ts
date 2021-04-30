77a787a9bb24e871a847c203302b872afddd0d26
Adiber <adiber.yt@gmail.com>
2021-04-30 15:59:53 +0200

    :bug: added Cors, no bean.db pushed

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import {Category, Challenge, CTF} from './models/index';
 import { resolve } from 'node:path';
@@ -27,7 +28,11 @@ const getUniqueID = () => {
 // I'm maintaining all active connections in this object
 const clients = {};
 // I'm maintaining all active users in this object
-const users = {};
+interface User {
+  id: string;
+  username: string;
+}
+const users:User[] = [];
 
 const sendMessage = (json) => {
   // We are sending the current data to all connected clients
@@ -57,7 +62,7 @@ wsServer.on('request', function(request) {
       const dataFromClient = JSON.parse(message.utf8Data);
       const json: wsResult = { type: dataFromClient.type, data: null };
       if (dataFromClient.type === wsTypes.USER_EVENT) {
-        users[userID] = dataFromClient;
+        users.push({id: userID, username: dataFromClient.username});
         json.data = users;
       }
       sendMessage(JSON.stringify(json));
@@ -68,7 +73,7 @@ wsServer.on('request', function(request) {
     console.log((new Date()) + " Peer " + userID + " disconnected.");
     const json = { type: wsTypes.USER_EVENT, data: users };
     delete clients[userID];
-    delete users[userID];
+    users.splice(users.findIndex(u => u.id === userID),1);
     sendMessage(JSON.stringify(json));
   });
 });
@@ -78,6 +83,9 @@ wsServer.on('request', function(request) {
 */ 
 const app = express();
 const expressPort = 9999
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
